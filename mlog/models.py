a69ef30f52fffcb511e9f7a21623adec15194c3b
Murat Ayfer <murat@ayfer.net>
2011-11-11 18:21:43 -0800

    new hover images for landing page

--- mlog/models.py
+++ mlog/models.py
@@ -5,8 +5,10 @@ class Entry(models.Model):
 		db_table = 'entries'
 	id = models.IntegerField(primary_key=True)
 	title = models.TextField()
+	url_title = models.CharField(max_length=100)
 	content = models.TextField()
+	custom_css = models.TextField()
 	date = models.DateField(auto_now_add=True)
 	date_label = models.CharField(max_length=100)
 	def __unicode__(self):
-		return self.title
+		return self.url_title
