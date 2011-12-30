ac7004265e9c8cd71cfd24b6f39503b0945566c8
Murat Ayfer <murat@ayfer.net>
2011-12-29 19:13:42 -0800

    simpler static file serving

--- urls.py
+++ urls.py
@@ -26,11 +26,8 @@ urlpatterns = patterns('',
 
 # to serve static files on development servers
 if settings.DEBUG:
-    from django.views.static import serve
-    _media_url = settings.MEDIA_URL
-    if _media_url.startswith('/'):
-        _media_url = _media_url[1:]
-        urlpatterns += patterns('',
-            (r'^%s(?P<path>.*)$' % _media_url, serve, {'document_root': settings.MEDIA_ROOT})
-        )
-    del(_media_url, serve)
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
