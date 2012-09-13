5e3850acbdc499209c84ac007c41e288c1ddafa2
Murat Ayfer <murat@ayfer.net>
2012-09-13 00:52:37 -0700

    untrack bots

--- mtracker/middleware.py
+++ mtracker/middleware.py
@@ -12,7 +12,18 @@ class TrackerMiddleware(object):
             '/favicon.ico',
             '/robots.txt'
         ]
+        bots = [
+            'Googlebot', 'Google-Test', 'Feedfetcher-Google',
+            'Baiduspider',
+            'bingbot'
+            'facebookexternalhit',
+            'YandexBot',
+            'JikeSpider',
+        ]
         track = True
+        for bot in bots:
+            if bot in visit.useragent:
+                track = False
         for path in blacklist:
             if visit.page.startswith(path):
                 track = False
