03285e1877ef0f369c815afec86dea4330a53650
Murat Ayfer <murat@ayfer.net>
2012-10-17 02:52:04 -0700

    remove unnecessary

--- experiments/tonehack/waves.js
+++ experiments/tonehack/waves.js
@@ -1,28 +1,3 @@
-window.requestAnimFrame = (function(){
-    return window.requestAnimationFrame || 
-        window.webkitRequestAnimationFrame || 
-        window.mozRequestAnimationFrame || 
-        window.oRequestAnimationFrame || 
-        window.msRequestAnimationFrame || 
-        function(callback, element){
-            return window.setTimeout(callback, 1000 / 60);
-        };
-})();
-window.cancelAnimFrame = (function(){
-    return window.cancelAnimationFrame || 
-        window.cancelRequestAnimationFrame || 
-        window.webkitCancelAnimationFrame || 
-        window.webkitCancelRequestFrame || 
-        window.mozCancelAnimationFrame || 
-        window.mozCancelRequestAnimationFrame || 
-        window.oCancelAnimationFrame || 
-        window.oCancelRequestAnimationFrame || 
-        window.msCancelAnimationFrame || 
-        window.msCancelRequestAnimationFrame || 
-        function(handler){
-            clearTimeout(handler);
-        };
-})();
 
 var X_INCREMENT = 10;
 var DEFAULT_SPEED = 3;
