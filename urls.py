64029fd6ad99efd542ecf521be281bd13a123f7b
Murat Ayfer <murat@ayfer.net>
2013-02-03 04:44:25 -0800

    small

--- urls.py
+++ urls.py
@@ -21,5 +21,5 @@ urlpatterns = patterns('',
     url(r'^make/?$', 'm.views.make', name='make'),
     
     url(r'^morphin/', include('m.morphin.urls', namespace='morphin')),
-    url(r'^chat/', include('localchat.urls', namespace='chat')),
+    url(r'^chat/', include('m.localchat.urls', namespace='chat')),
 )
