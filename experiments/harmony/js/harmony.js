c7f6ff658ebae4586ab0ca2ebd136d880a4947e8
Murat Ayfer <murat@ayfer.net>
2013-11-20 01:09:23 -0800

    new drawings

--- experiments/harmony/js/harmony.js
+++ experiments/harmony/js/harmony.js
@@ -16,8 +16,15 @@ Harmony = {
         var matches = [];
         var chord_overtones = Harmony.get_overtones_of_chord(chord);
 
-        for(var i=0; i < 88; i++) {
-            var note_overtones = Harmony.get_overtones_of_chord([i]);
+        var all_notes = []; //normally would use var here
+        n = 0;  //normally would use var here
+        while(all_notes.push(n++)<87){}
+        //console.log('notes', all_notes);
+        var all_chords = Harmony.combine(all_notes, 3, 3);
+        //console.log(all_chords.length, all_chords);
+
+        for(var i=0; i < all_chords.length; i++) {
+            var note_overtones = Harmony.get_overtones_of_chord(all_chords[i]);
             var scores = [];
             for(var x=0; x < chord_overtones.length; x++) {
                 for(var y=0; y < note_overtones.length; y++) {
@@ -34,12 +41,37 @@ Harmony = {
             }
 
             var score = score_sum/scores.length;
+            var match_chord = [];
+            for(var r=0; r<all_chords[i].length; r++) {
+                match_chord[r] = Chords.halftone_to_note(all_chords[i]);
+            }
             matches.push({
                 'score': score,
-                'match': Chords.halftone_to_note(i),
+                'match': match_chord,
             });
         }
         matches.sort(function(a, b){ return b.score - a.score; });
         return matches;
+    },
+
+    combine: function(a, min, max) {
+        var fn = function(n, src, got, all) {
+            if (n == 0) {
+                if (got.length > 0) {
+                    all[all.length] = got;
+                }
+                return;
+            }
+            for (var j = 0; j < src.length; j++) {
+                fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+            }
+            return;
+        }
+        var all = [];
+        for (var i = min; i <= max; i++) {
+            fn(i, a, [], all);
+        }
+        all.push(a);
+        return all;
     },
 }
