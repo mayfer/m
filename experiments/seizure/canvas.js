665a08a742377e146b2fb17cc7f374d7d4e6eb62
Murat Ayfer <murat@ayfer.net>
2014-04-23 20:49:08 -0700

    stuff

--- experiments/seizure/canvas.js
+++ experiments/seizure/canvas.js
@@ -3,16 +3,16 @@ function Canvas(jq_elem) {
     // create a canvas inside another element
     // and set the height&width to fill the element
     var canvas_jq = $('<canvas>');
-    var canvas = canvas_jq.get(0);
-    var width = jq_elem.innerWidth();
-    var height = jq_elem.innerHeight();
+    this.canvas = canvas_jq.get(0);
+    this.width = jq_elem.innerWidth();
+    this.height = jq_elem.innerHeight();
     
-    canvas_jq.attr('width', width);
-    canvas_jq.attr('height', height);
-    var context = canvas.getContext("2d");
+    canvas_jq.attr('width', this.width);
+    canvas_jq.attr('height', this.height);
+    this.context = this.canvas.getContext("2d");
     // make the h/w accessible from context obj as well
-    context.width = width;
-    context.height = height;
+    context.width = this.width;
+    context.height = this.height;
     canvas_jq.appendTo(jq_elem);
     return canvas_jq;
 }
